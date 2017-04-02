713ff74d4265911ed6ac7a6e478540d4c21fbdd7
maxengel99 <maxengel99@gmail.com>
2017-04-02 03:11:23 -0500

    Added design to landing page

--- src/client/views/landing.js
+++ src/client/views/landing.js
@@ -7,6 +7,7 @@ import React from 'react';
 import { browserHistory } from 'react-router';
 import GoogleLogin from 'react-google-login';
 import RaisedButton from 'material-ui/RaisedButton'
+import {Card, CardActions, CardHeader, CardMedia, CardTitle, CardText} from 'material-ui/Card';
 
 export class Landing extends React.Component {
     constructor(props) {
@@ -101,6 +102,19 @@ export class Landing extends React.Component {
     render() {
         return (
             <div>
+            <Card>
+              <CardHeader title="Think Big Picture" avatar="https://s-media-cache-ak0.pinimg.com/736x/27/6b/92/276b9282169e378ee334b5184b0bb182.jpg" />
+              <CardMedia overlay={<CardTitle title="Teamwork, Community, Family" />}>
+              <img src="http://gender.stanford.edu/sites/default/files/uploads/2010/04/Teamwork-iStock_000008295703Medium.jpg" style={{height:'80%',width:'60%'}} />
+              </CardMedia>
+              <CardText><p style={{fontSize:'300%', fontFamily:'Corben'}}>
+                <b>WishWash</b> believes that connections build the foundation for
+                succesful communities. With more and more things going on in our lives,
+                it is becoming increasingly important to develop our network.
+                Our goal is to build those connections through a shared commonality
+                of college laundry.</p>
+              </CardText>
+            </Card>
                 <div id="my-signin2"></div>
                 <RaisedButton label='Sign Out' onTouchTap={this.signOut.bind(this)}></RaisedButton>
             </div>
